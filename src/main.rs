646d36c16240bd9088fad13680a8c50f0a984697
Daniel Müller <deso@posteo.net>
2025-03-26 21:47:20 -0700

    Support parsing durations in different units
    So far our backoff options always required the time to be specified in
    milliseconds. That can be cumbersome to do and is not particularly
    user-friendly.
    Add support for recognizing the ms, s, and m suffixes, denominating
    milliseconds, seconds, and minutes, respectively.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::time::Instant;
 
+use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Error;
 
@@ -26,6 +27,28 @@ use log::error;
 use clap::Parser;
 
 
+/// Parse a duration from a string.
+fn parse_duration(s: &str) -> Result<Duration, Error> {
+  let durations = [
+    ("ms", 1),
+    ("sec", 1000),
+    ("s", 1000),
+    ("min", 60000),
+    ("m", 60000),
+  ];
+
+  for (suffix, multiplier) in &durations {
+    if let Some(base) = s.strip_suffix(suffix) {
+      if let Ok(count) = base.parse::<u64>() {
+        return Ok(Duration::from_millis(count * multiplier))
+      }
+    }
+  }
+
+  Err(anyhow!("invalid duration provided: {}", s))
+}
+
+
 /// A program for "watched" invocation of others.
 #[derive(Debug, Parser)]
 struct Args {
@@ -33,19 +56,25 @@ struct Args {
   command: PathBuf,
   /// The managed program's arguments.
   arguments: Vec<OsString>,
-  /// The backoff time in milliseconds to use initially.
+  /// The backoff time to use initially.
   ///
   /// This value also acts as the minimum time the program needs to be
   /// alive in order to not increase the backoff.
-  #[arg(long = "backoff-millis-base", default_value = "100")]
-  backoff_millis_base: u64,
+  ///
+  /// Accepted suffixes are 'ms' for milliseconds, 's' for seconds, and
+  /// 'm' for minutes.
+  #[arg(long = "backoff-base", value_parser = parse_duration, default_value = "100ms")]
+  backoff_base: Duration,
   /// The factor to multiply the current backoff with to get the next
   /// backoff.
   #[arg(long = "backoff-multiplier", default_value = "2.0")]
   backoff_muliplier: f64,
-  /// The maximum backoff (in milliseconds) to use.
-  #[arg(long = "backoff-max", default_value = "30000")]
-  backoff_millis_max: u64,
+  /// The maximum backoff to use.
+  ///
+  /// Accepted suffixes are 'ms' for milliseconds, 's' for seconds, and
+  /// 'm' for minutes.
+  #[arg(long = "backoff-max", value_parser = parse_duration, default_value = "30s")]
+  backoff_max: Duration,
 }
 
 
@@ -124,8 +153,33 @@ fn main() -> ! {
   watchdog(
     &args.command,
     &args.arguments,
-    Duration::from_millis(args.backoff_millis_base),
+    args.backoff_base,
     args.backoff_muliplier,
-    Duration::from_millis(args.backoff_millis_max),
+    args.backoff_max,
   );
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+
+  /// Check that we can parse duration descriptions properly.
+  #[test]
+  fn duration_parsing() {
+    assert_eq!(parse_duration("1ms").unwrap(), Duration::from_millis(1));
+    assert_eq!(parse_duration("500ms").unwrap(), Duration::from_millis(500));
+    assert_eq!(parse_duration("1s").unwrap(), Duration::from_secs(1));
+    assert_eq!(parse_duration("1sec").unwrap(), Duration::from_secs(1));
+    assert_eq!(parse_duration("13s").unwrap(), Duration::from_secs(13));
+    assert_eq!(parse_duration("13sec").unwrap(), Duration::from_secs(13));
+    assert_eq!(parse_duration("1m").unwrap(), Duration::from_secs(60));
+    assert_eq!(parse_duration("1min").unwrap(), Duration::from_secs(60));
+    assert_eq!(parse_duration("13m").unwrap(), Duration::from_secs(13 * 60));
+    assert_eq!(
+      parse_duration("13min").unwrap(),
+      Duration::from_secs(13 * 60)
+    );
+  }
 }
